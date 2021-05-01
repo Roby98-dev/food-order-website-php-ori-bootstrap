ae64ef3822e0d25cd3464f0e2e87439ca1033d62
I KADEK ROBY ADI PUTRA <73769308+Roby98-dev@users.noreply.github.com>
2021-05-01 22:24:04 +0800

    First commit

---
+++ admin/partials/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer Section Starts -->
+<div class="footer bg-dark">
+    <div class="wrapper">
+        <p class="mt-3 text-center"><?php date('Y'); ?> All rights reserved, Poinsal Food House. Developed By | <a href="https://poinsla.com">Poinsla.com</a></p>
+    </div>
+</div>
+<!-- Footer Section Ends -->
+
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
